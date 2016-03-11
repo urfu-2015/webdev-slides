24b7a89a51af16f6d4524d28ad3492f5edbf2c80
Sergey Gogolev <gogoleff@yandex-team.ru>
2016-03-11 13:18:38 +0500

    Пример перенесён в отдельный репозиторий

--- 5-deploy/examples/controllers/pages.js
+++
@@ -1,9 +0,0 @@
-exports.index = (req, res) => {
-    const data = {
-        message: 'Hello, User!'
-    };
-
-    res.render('main', Object.assign(data, req.commonData));
-};
-
-exports.error404 = (req, res) => res.sendStatus(404);
